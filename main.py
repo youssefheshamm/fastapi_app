3b109047d3fddb5171c43ee18b9e1a2d5f12981d
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-16 20:41:23 +0300

    Simple FASTAPI addition and endpoint

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"This is the root endpoint😎"}
+
+@app.get("/add/{a}/{b}")
+def add_numbers(a, b):
+    try:
+        a_num = float(a)
+        b_num = float(b)
+        result = a_num + b_num
+        if a_num.is_integer():
+            a_num = int(a_num)
+        if b_num.is_integer():
+          b_num = int(b_num)
+        if result.is_integer():
+           result = int(result)
+        return {f"a = {a_num}, b = {b_num}, a + b = {result} "}
+    except ValueError:
+        return {"ERROR: both inputs must be numeric values"}
+
+

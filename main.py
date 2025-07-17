adc8742ce33c6e8af6583d47b2cd1c4bf50911f3
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-17 14:47:09 +0300

    Remove pycache

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return {"This is the root endpoint😎"}
+
 
 @app.get("/add/{a}/{b}")
 def add_numbers(a, b):
@@ -15,11 +17,9 @@ def add_numbers(a, b):
         if a_num.is_integer():
             a_num = int(a_num)
         if b_num.is_integer():
-          b_num = int(b_num)
+            b_num = int(b_num)
         if result.is_integer():
-           result = int(result)
+            result = int(result)
         return {f"a = {a_num}, b = {b_num}, a + b = {result} "}
     except ValueError:
         return {"ERROR: both inputs must be numeric values"}
-
-

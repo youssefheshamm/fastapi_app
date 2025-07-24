adc7ee8d3452f933148c0b55f910b01923c44226
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-24 03:26:47 +0300

    feat(task): add task creation, storage, and retrieval with 404 HTTPException handling

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import HTTPException
 
 app = FastAPI()
+
+
+class Task(BaseModel):
+    title: str
+    description: str = None
+
+
+tasks = []
 
 
 @app.get("/")
@@ -42,3 +52,17 @@ def palindrome(x):
             return {"The word is a palindrome"}
         else:
             return {"The word is too short"}
+
+
+@app.post("/task/")
+def task_list(task: Task):
+    tasks.append(task)
+    return {f"Task recieved and stored: {task} "}
+
+
+@app.get("/task/{title}")
+def get_task(title: str):
+    for task in tasks:
+        if task.title.lower() == title.lower():
+            return {f"Task: {task}"}
+    raise HTTPException(status_code=404, detail="Task not found")

8f395f1d4482ed55ee07818ca9e913ee9cbae6d4
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-27 17:57:31 +0300

    feat: prevent duplicate task titles

--- main.py
+++ main.py
@@ -56,6 +56,10 @@ def palindrome(x):
 
 @app.post("/task/")
 def task_list(task: Task):
+    for x in tasks:
+        if x.title.lower() == task.title.lower():
+            raise HTTPException(
+                status_code=400, detail=f"The title {task.title} is already used. Please try another one")
     tasks.append(task)
     return {f"Task recieved and stored: {task} "}
 
